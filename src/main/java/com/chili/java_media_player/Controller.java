622e8f8f4135dd6193addf862165605db5e21dc0
Logan Small <lsmall@sleepycat.me>
2025-10-24 11:51:45 -0400

    Add proof of concepts

---
+++ src/main/java/com/chili/java_media_player/Controller.java
@@ -0,0 +1,14 @@
+package com.chili.java_media_player;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to Java Media Player & Visualizer!");
+    }
+}
