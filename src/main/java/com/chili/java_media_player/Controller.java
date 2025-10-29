7ff2d34b0162bc8e235add844b03a61678254650
Yong Huang <yong48436@gmail.com>
2025-10-29 14:17:55 -0400

    added the ability for the button to generate a 50/50 between two welcome messages

--- src/main/java/com/chili/java_media_player/Controller.java
+++ src/main/java/com/chili/java_media_player/Controller.java
@@ -1,5 +1,9 @@
 package com.chili.java_media_player;
 
+import java.util.ArrayList;
+import java.util.Collections; //imported here to add a shuffle, can remove later if shuffle not required
+import java.util.List;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -9,6 +13,13 @@ public class Controller {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to Java Media Player & Visualizer!");
+
+        List<String> welcomeList = new ArrayList<>();
+        welcomeList.add("Welcome to Java Media Player & Visualizer!");
+        welcomeList.add("Welcome to JMD!");
+        Collections.shuffle(welcomeList);
+
+        welcomeText.setText(welcomeList.get(0));
+
     }
 }
