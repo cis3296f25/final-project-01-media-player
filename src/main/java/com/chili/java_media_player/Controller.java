c14497f4fd831e7ce642cda2ecf384e78cfe25c8
Joseph-A-F <149717113+Joseph-A-F@users.noreply.github.com>
2025-10-30 17:13:30 -0400

    implementing a test progress bar controlled via an Animation timer

--- src/main/java/com/chili/java_media_player/Controller.java
+++ src/main/java/com/chili/java_media_player/Controller.java
@@ -4,12 +4,37 @@ import java.util.ArrayList;
 import java.util.Collections; //imported here to add a shuffle, can remove later if shuffle not required
 import java.util.List;
 
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
 
 public class Controller {
     @FXML
     private Label welcomeText;
+    @FXML
+    private ProgressIndicator testProgressBar;
+
+    private AnimationTimer testProgressBarTimer;
+    private long startTime;
+
+    // private long star
+    @FXML
+    private void initialize() {
+        this.startTime = System.nanoTime();
+
+        this.testProgressBarTimer = new AnimationTimer() {
+
+            @Override
+            public void handle(long now) {
+                double elapsedSeconds = (now - startTime) / 1_000_000_000.0;
+                double progress = (elapsedSeconds % 5) / 5.0; // Loops every 5 seconds
+                testProgressBar.setProgress(progress);
+                // testProgressBar.setProgress(now(double));
+            }
+        };
+        testProgressBarTimer.start();
+    }
 
     @FXML
     protected void onHelloButtonClick() {
@@ -22,4 +47,5 @@ public class Controller {
         welcomeText.setText(welcomeList.get(0));
 
     }
+
 }
