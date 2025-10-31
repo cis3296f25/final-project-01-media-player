7384ef5c1c6f585829756f7123d7b1adc00ff9c6
Back-Gammon <129220958+Back-Gammon@users.noreply.github.com>
2025-10-31 09:45:53 -0400

    Added Author Credit List

--- src/main/java/com/chili/java_media_player/JavaMediaPlayer.java
+++ src/main/java/com/chili/java_media_player/JavaMediaPlayer.java
@@ -12,7 +12,7 @@ public class JavaMediaPlayer extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(JavaMediaPlayer.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 320*3, 240*3);
         stage.setTitle("Java Media Player & Audio Visualizer");
         stage.setScene(scene);
         stage.show();
